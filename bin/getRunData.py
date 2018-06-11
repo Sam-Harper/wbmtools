724d521d9c2d434d4f55eea3f791079de0b72d66
Sam Harper <sam.j.harper@gmail.com>
2018-06-11 09:19:37 +0100

    now has update options

--- bin/getRunData.py
+++ bin/getRunData.py
@@ -3,7 +3,7 @@ import sys
 import math
 import time
 import json
-from termcolor import colored
+import os
 import wbmtools.wbmutil as wbmutil
 from wbmtools.wbmparser import WBMParser
 
@@ -23,12 +23,34 @@ def convert_to_ranges(vals):
     ranges.append([start_val,prev_val])
     return ranges
 
+class FileSuffexes:
+    def __init__(self):
+        self.run_info = "_runInfo.json"
+        self.l1_prescales = "_l1prescales.json"
+        self.hlt_prescales = "_hltprescales.json"
+
+def read_json_data(file_name,args):
+    if args.update and args.recreate:
+        print "--update and --recreate are mutually exclusive options"
+        sys.exit() 
+    json_data={}
+    if args.update and os.path.isfile(file_name):
+        with open(file_name) as f:
+            json_data = json.load(f)
+    elif args.update and not os.path.isfile(file_name):
+        print "--update option chosen but file ",file_name,"does not exist"
+        sys.exit()
+    return json_data
+        
+            
 
 import argparse
 parser = argparse.ArgumentParser(description='dumps informatation about the runs ')
 parser.add_argument('--runs',required=False,nargs="+",help='runs to output')
 parser.add_argument('--grl',required=False,help='good lumi list')
 parser.add_argument('--out_basename','-o',help='output base name (outputs <out_basename>_runInfo.json, <out_basename>_l1prescales.json, <out_basename>_hltprescales.json',required=True)
+parser.add_argument('--update','-u',action='store_true',help='updates an existing files')
+parser.add_argument('--recreate',action='store_true',help='re-creates an existing file')
 args = parser.parse_args()
 
 wbmparser=WBMParser()
@@ -48,15 +70,18 @@ if runs == []:
     runs=wbmutil.get_runs_from_fills("2018.01.01","2019.01.01",wbmparser)
 runs.sort()
 
-runs_data = {}
-l1_prescale_data = {}
-hlt_prescale_data = {}
+runs_data = read_json_data(args.out_basename+FileSuffexes().run_info,args)
+l1_prescale_data = read_json_data(args.out_basename+FileSuffexes().l1_prescales,args)
+hlt_prescale_data = read_json_data(args.out_basename+FileSuffexes().hlt_prescales,args)
+
+
 bad_runs = []
 bad_runs_lumi = []
 nr_runs = len(runs)
 pre_time = time.time()
 
 for index,run in enumerate(runs):
+    if run in runs_data: continue
     cur_time = time.time()
     print "processing {} ({}/{}), time taken: {:.1f}".format(run,index+1,nr_runs,cur_time-pre_time)
     pre_time=cur_time
@@ -102,12 +127,12 @@ for index,run in enumerate(runs):
         hlt_prescale_data[hlt_menu] = hlt_prescale_set
 
 
-import json
-with open(args.out_basename+'_runInfo.json', 'w') as outfile:
+
+with open(args.out_basename+FileSuffexes().run_info, 'w') as outfile:
     json.dump(runs_data, outfile,sort_keys = True)
-with open(args.out_basename+'_l1prescales.json', 'w') as outfile:
+with open(args.out_basename+FileSuffexes().l1_prescales, 'w') as outfile:
     json.dump(l1_prescale_data, outfile)
-with open(args.out_basename+'_hltprescales.json', 'w') as outfile:
+with open(args.out_basename+FileSuffexes().hlt_prescales, 'w') as outfile:
     json.dump(hlt_prescale_data, outfile)
 
 print "badruns: ",bad_runs
